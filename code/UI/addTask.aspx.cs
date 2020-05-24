bf7edf9840b50a04bf37bb1bd1d2994b9ee96bf7
Ye_Bob <50488319+17373176@users.noreply.github.com>
2020-05-24 22:48:12 +0800

    Update addTask.aspx.cs

--- code/UI/addTask.aspx.cs
+++ code/UI/addTask.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace our.ui
 {
+    //预定任务的相应任务与功能模块的接口对应
     public partial class addTask1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
